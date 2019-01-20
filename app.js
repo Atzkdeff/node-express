8fde1f808ee7fc1aa6a8f4b7413d3de2af42c8e5
Andrei Shyliahin <a.shyliahin@gmail.com>
2019-01-20 18:18:18 +0300

    feat: implement logging

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let sassMiddleware = require('node-sass-middleware');
+let winston = require('winston');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var newsRouter = require('./routes/news');
-var news_json = require('./data/news');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let newsRouter = require('./routes/news');
+let news_json = require('./data/news');
 
-var app = express();
+const loggerToFile = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        //
+        // - Write to all logs with level `info` and below to `combined.log`
+        // - Write all logs error (and below) to `error.log`.
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    loggerToFile.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use((req, res, next) => {
+    loggerToFile.log("info",
+        `Time: ${new Date().toISOString()}
+        URL: ${req.originalUrl}`
+        );
+    next()
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,17 +65,17 @@ app.use('/users', usersRouter);
 app.use('/news', newsRouter);
 
 
-// app.use((req, res) => {
-//   res.send(news_json)
-// });
+app.use((req, res) => {
+  res.send(news_json)
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/news.js
+++ routes/news.js
@@ -1,33 +1,33 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET news listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
     res.send('GET /news');
     res.end()
   }
 );
 
 /* GET news with id listing. */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', (req, res, next) => {
   console.log("GET /news/{id}", req.params.id);
   res.end()
 });
 
 /* POST news listing. */
-router.post('/', function(req, res, next) {
+router.post('/', (req, res, next) => {
   console.log("POST /news");
   res.end()
 });
 
 /* PUT news with id listing. */
-router.put('/:id', function(req, res, next) {
+router.put('/:id', (req, res, next) => {
   console.log("PUT /news", req.params.id);
   res.end()
 });
 
 /* DELETE news with id listing. */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', (req, res, next) => {
   console.log("DELETE /news/{id} ", req.params.id);
   res.end()
 });
